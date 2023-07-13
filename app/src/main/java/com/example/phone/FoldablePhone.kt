4b0e9001824ccd070f5bbe6e618f86e9c6f16f86
LuisFuentesDev <lsfuentes2@gmail.com>
2023-07-12 21:50:11 -0400

    pantalla encendida y celular doblado

---
+++ app/src/main/java/com/example/phone/FoldablePhone.kt
@@ -0,0 +1,19 @@
+package com.example.phone
+
+class FoldablePhone(isScreenLightOn: Boolean = false) : Phone(isScreenLightOn) {
+
+    var isFoldable: Boolean = false
+    override fun switchOn() {
+        if (!isFoldable) {
+            isScreenLightOn = true
+        }
+    }
+        fun abrir() {
+            isFoldable = true
+        }
+
+        fun cerrar() {
+            isFoldable = false
+        }
+    }
+
